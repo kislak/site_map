317485297e48ed713e2c8d0a195d8254e2d740cd
serge <kislak7@gmail.com>
2019-09-20 00:57:26 +0300

    Add annotation

--- app/models/site_map_node.rb
+++ app/models/site_map_node.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: site_map_nodes
+#
+#  id         :integer          not null, primary key
+#  link_name  :string
+#  path       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class SiteMapNode < ApplicationRecord
 end
--- spec/models/site_map_node_spec.rb
+++ spec/models/site_map_node_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: site_map_nodes
+#
+#  id         :integer          not null, primary key
+#  link_name  :string
+#  path       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe SiteMapNode, type: :model do
